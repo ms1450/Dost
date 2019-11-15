741511098ea3637fac41d2dc60e093bd5732bf99
ms1450 <ms1450@rit.edu>
2019-11-15 17:38:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dost/Constants.java
@@ -0,0 +1,6 @@
+package com.example.dost;
+
+public class Constants {
+    public static final String openChannelType = "Open";
+    public static final String groupChannelType = "Group";
+}
---
+++ app/src/main/java/com/example/dost/MainMenuActivity.java
@@ -0,0 +1,42 @@
+package com.example.dost;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.dost.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+    String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        userID = getIntent().getStringExtra("userID");
+        LinearLayout start_group_channel_list_activity = findViewById(R.id.start_group_channel_list_activity);
+        start_group_channel_list_activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, ChannelListActivity.class);
+                intent.putExtra("userID", userID);
+                intent.putExtra("channelType", Constants.groupChannelType);
+                startActivity(intent);
+            }
+        });
+
+        LinearLayout start_open_channel_list_activity = findViewById(R.id.start_open_channel_list_activity);
+        start_open_channel_list_activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, ChannelListActivity.class);
+                intent.putExtra("userID", userID);
+                intent.putExtra("channelType", Constants.openChannelType);
+                startActivity(intent);
+            }
+        });
+    }
+}
